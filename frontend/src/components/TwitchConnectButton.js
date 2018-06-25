369b5437ba1929518cfa72c46c923d758faf8576
Leo <leorojas22@gmail.com>
2018-06-24 21:39:11 -0400

    fix

--- frontend/src/components/TwitchConnectButton.js
+++ frontend/src/components/TwitchConnectButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TwitchConnectButton = (props) => (
 	<div className="text-center">
 		<p>Connect with Twitch to Vote!</p>
-		<a className="twitch-connect-btn" href="http://api.yogscinemavote.com/twitch-authenticate">
+		<a className="twitch-connect-btn" href="https://api.yogscinemavote.com/twitch-authenticate">
 			<img alt="Twitch Icon" src="/images/twitch-icon.png" /> <span>Connect with Twitch</span>
 		</a>
 	</div>
