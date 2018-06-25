56fbc51a18c09c2279b31d6ac68c13cd078469ca
Leo <leorojas22@gmail.com>
2018-06-24 23:17:30 -0400

    typo

--- frontend/src/components/Disclaimer.js
+++ frontend/src/components/Disclaimer.js
@@ -7,7 +7,7 @@ const Disclaimer = (props) => (
 			If you find any bugs or want to leave feedback, message me on reddit: <a href="https://www.reddit.com/u/lolparodyaccounts" target="_blank" rel="noopener noreferrer">/u/lolparodyaccounts</a> 
 		</p>
 		<p>
-		<strong>Note:</strong> If you connect to this app and would like to fully disconnect, <a href="https://www.twitch.tv/settings/connections" target="_blank" rel="noopener noreferrer">Click Here</a>.  Scroll down to "Yogscinema Vote" and then click Disconnect.
+		<strong>Note:</strong> If you connect to this app and would like to fully disconnect, <a href="https://www.twitch.tv/settings/connections" target="_blank" rel="noopener noreferrer">Click Here</a>.  Scroll down to "YogsCinema Vote" and then click Disconnect.
 		</p>
 		<p>
 			<strong>Credits:</strong> <a href="https://yogsdb.com" rel="noopener noreferrer" target="_blank">YogsDB.com</a> for the info in the custom votes section
